45dbbaa3466efe0b41f4f58169eec35867921c54
joshuawisdom <78412743+joshuawisdom@users.noreply.github.com>
2021-03-12 21:48:34 -0800

    Added header comments

--- Assets/Scripts/CombinationLoader.cs
+++ Assets/Scripts/CombinationLoader.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections;
+﻿//Joshua Wisdom
+//2313991
+//CPSC 236-03
+//jowisdom@chapman.edu
+//Project 03: Keypad
+//This is my own work, and I did not cheat on this assignment.
+
+//No known errors.
+
+//Completed alongside provided videos.
+
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Keypad
+# JoshuaWisdom_Keypad
  
+Joshua Wisdom
+2313991
+CPSC 236-03
+jowisdom@chapman.edu
+Project 03: Keypad
+This is my own work, and I did not cheat on this assignment.
+
+No known errors.
+
+Completed alongside provided videos.
